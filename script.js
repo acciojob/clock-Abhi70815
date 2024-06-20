6f1dedf1a47f9d56a4584569a42087e9c2e58fb2
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-20 20:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timer=document.getElementById('timer');
+let date= new Date();
+setInterval(
+	funtion(){
+date=new Date();
+	timer.innerHTML=date.toLocaleString();
+},1000)
+// timer.innerHTML=date.toLocaleString();
